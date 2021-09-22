a275191991c2cb059e3c0a8ebcd521a8fb7ff53e
Rishi P <rishi.holmes@gmail.com>
2021-09-23 01:05:25 +0530

    final

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,9 +1,12 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
 
 import classes from './MainNavigation.module.css';
+import FavoritesContext from "../../store/favorites-context";
  
 
 function MainNavigation() {
+    const favoritesCtx= useContext(FavoritesContext);
     return (
         <header className={classes.header}>
         <div className = {classes.logo}>React Meetups</div>
@@ -16,7 +19,8 @@ function MainNavigation() {
         <Link to='/new-meetup'>New Meetup</Link>
         </li>
         <li>
-        <Link to='/favorites'>Favorites</Link>
+        <Link to='/favorites'>Favorites
+        <span className={classes.badge}>{favoritesCtx.totalFavorites}</span></Link>
         </li>
         </ul>
         </nav>
--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,23 @@
+import { useContext } from 'react';
+
+import FavoritesContext from '../store/favorites-context';
+
+import MeetupList from '../components/meetups/MeetupList';
+
 function Favorites(){
-    return <div>Favorites</div>
+    const favoritesCtx = useContext(FavoritesContext)
+
+let content;
+if (favoritesCtx.totalFavorites===0) {
+    content = <p>You have no favorites yet. Start adding now!!</p>;
+} else {
+    content = <MeetupList meetups={favoritesCtx.favorites} />;
+}
+
+    return <section> 
+        <h1>Favorites</h1>
+        {content}
+        </section>
 }
 
 
